f2b97bba3b2259dc1f3e95fb5ea2b9c4e1a84652
HaileySanders82 <103207122+HaileySanders82@users.noreply.github.com>
2024-01-17 16:14:16 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Mission2Assignment
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            //Intro message and prompt for input
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+
+            Console.Write("How many dice rolls would you like to simulate? ");
+
+            //numRolls= whatever the user put (turned into an int)
+            int numRolls = int.Parse(Console.ReadLine());
+
+            //Create a RollDice object called diceSimulator
+            RollDice diceSimulator = new RollDice();
+
+            //Put the results into an array of integers called 'results' by calling the SimulateRolls method from the diceSimulator object
+            int[] results = diceSimulator.SimulateRolls(numRolls);
+
+            //ouptut messages
+            Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+
+            //calling the DisplayResults method on the results array
+            DisplayResults(results);
+
+            //Goodbye!
+            Console.WriteLine($"\nThank you for using the dice throwing simulator. Goodbye!");
+        }
+
+        //Creating the DisplayResults method
+        private static void DisplayResults(int[] results)
+        {
+            //a totalRolls integer variable is the number of items in the results array
+            int totalRolls = results.Length;
+
+            //Write the message displaying the number of rolls found in the totalRolls variable
+            Console.WriteLine($"Total number of rolls = {totalRolls}.");
+
+            //initializing the for loop
+            for (int i = 2; i <= 12; i++)
+            {
+                //calling the CountOccurrences method, passing in i and the results array
+                int count = CountOccurrences(results, i);
+                //Calculates the percentage
+                int percentage = (count * 100) / totalRolls;
+
+                //Adds a * for each percentage point
+                Console.WriteLine($"{i}: {new string('*', percentage)}");
+            }
+        }
+
+        //Creating the CountOccurrences method
+        private static int CountOccurrences(int[] array, int value)
+        {
+            // A variable called count is set to 0
+            int count = 0;
+
+            //Looping through each element in the array passed to find the number of times the sum of the roll == each number
+            foreach (int element in array)
+            {
+                if (element == value)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ RollDice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Mission2Assignment
+{
+    //Creating the RollDice class
+    internal class RollDice
+    {
+        //Creating the SimulateRolls method 
+        public int[] SimulateRolls(int numRolls)
+        {
+            //creating the results array with the length of the numRolls
+            int[] results = new int[numRolls];
+
+            //creating a new random object called random
+            Random random = new Random();
+
+            //Executing this loop the amount of times numRolls is
+            for (int i = 0; i < numRolls; i++)
+            {
+                //Getting a random number for each dice between 1 and 6
+                int dice1 = random.Next(1, 7);
+                int dice2 = random.Next(1, 7);
+
+                //adding the two random numbers together
+                int sum = dice1 + dice2;
+
+                //storing the sum in the results array at position i
+                results[i] = sum;
+            }
+
+            //sending back the results array
+            return results;
+        }
+    }
+}
+
